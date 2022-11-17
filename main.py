8697cb0570b7841a14ae2bce3e7cb1cbf8daac0e
bengisusimsek <bengisu.simsekk@gmail.com>
2022-11-17 10:42:24 +0300

    Step1

---
+++ main.py
@@ -0,0 +1,57 @@
+print("Merhaba Etiya")
+ # string = metinsel ifade 
+text ="10"
+print(text)
+
+#integer = tam sayı
+number = 5
+print(number)
+
+#double,float decimal= ondalıklı sayı
+dnumber = 5.3
+print(dnumber)
+
+print(dnumber)
+
+#! matematiksel operatörler 
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3) 
+
+# ! boolean, bool= true veya false
+isVerified = True 
+
+# mantıksal , karşılaştırma operatörler => her mantıksal operatör boolean değer döner 
+print(1 == 2)
+print(1 != 1)
+
+print(2 > 3 )
+print(2 < 3)
+
+print(2 < 2)
+print(2 > 2)
+
+print(2 <= 2)
+print(2 >= 2 )
+
+print(10%2==0)
+print(50/2 == 25.0)
+print(50/2 == 25)
+
+#string operasyonları 
+text ="Merhaba Etiya"
+print( text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.capitalize())
+
+name = "Halit"
+age = "23"
+company = "Kodlamaio"
+
+#Halit 23 yaşında Kodlamaio'da çalışıyor
+print(name + " " + age + " yaşında " + company + " de çalışıyor")
+print (f"{name} {age} yaşında {company} 'de çalışıyor")
+
---
+++ step.py
@@ -0,0 +1,21 @@
+baslik = "HABERİNİZ OLSUN" #string
+vade = 12  #integer
+faizOrani = 1.47 #float
+faizOrani2 = 1.44 
+
+
+print(baslik)
+print(type(baslik)) 
+print(type(vade))
+print(type(faizOrani))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Bengisu"
+musteriSoyadi = "Şimşek"
+sonucMesaj=(mesaj+" "+ musteriAdi +" "+ musteriSoyadi)
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2 )
+print(sonucMesaj)
